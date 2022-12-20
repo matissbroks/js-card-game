6a63aa407feba58dc1da25d0f2b82d2c8525b64e
matissbroks <matiss.broks@gmail.com>
2022-12-20 22:44:55 +0200

    Information after each move, show in playing area, same ar end game message. Fixed shuffling

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let hitButton = document.getElementById('hitCards')
 
+    let infoField = document.getElementById('info-field')
+
     let playerField = document.getElementById('playerField')
     let userField = document.getElementById('userField')
-
-    let userCardContainer = document.getElementById('userCardContainer')
-    let oponentCardContainer = document.getElementById('oponentCardContainer')
 
     let suits = ["spades", "diamonds", "clubs", "hearts"];
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -17,11 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let oponentCards = new Array();
     let drawCardsForTake = new Array();
 
-    let playerCardCount = 0;
-    let oponentCardCount = 0;
-
     startGame()
-
 
     function startGame() {
         let freshDeck = shuffle(createDeck())
@@ -29,70 +24,61 @@ document.addEventListener('DOMContentLoaded', function() {
         // dealCards(freshDeck);        // Deal all cards
         dealCards(freshDeck.slice(0, 10));  // Deal only first 10 cards
 
-        drawCards()
-
         let moveCount = 0;
 
         hitButton.addEventListener('click', ()=> {
             moveCount++;
             hitCards(userCards, oponentCards)
 
-            drawCards()
-
-            if(checkLoseWin()) {
-                hitButton.remove();
-                drawCards(true)
-                return;
-            }
+            playerCardCountText.innerText = userCards.length;
+            oponentCardCountText.innerText = oponentCards.length;
 
             if(moveCount == 5) {
-                alert("Shuffling cards!")
-
                 hitButton.classList.add("hide");
 
                 setTimeout(() => {
-                    hitButton.classList.remove("hide");
+                    clearFiledCards();
+                    showInfo("Shuffling cards!");
                 }, 1000);
+    
+                setTimeout(() => {
+                    hitButton.classList.remove("hide");
+                }, 3000);
                 
-                shuffle(userCards)
-                shuffle(oponentCards)
+                userCards = shuffle(userCards)
+                oponentCards = shuffle(oponentCards)
                 moveCount = 0
+            }
+
+            if(checkLoseWin()) {
+                hitButton.remove();
+                return;
             }
         })
     }
 
-    function drawCards(clear = false) {
-        if(clear) {
-            userCardContainer.innerHTML = ''
-            oponentCardContainer.innerHTML = ''
-            document.getElementById('playField').innerHTML = ''
-            return
+
+    function clearFiledCards() {
+        playerField.innerHTML = '';
+        userField.innerHTML = '';
+    }
+
+    function showInfo(message, withTimeout = true) {
+        infoField.classList.remove('hide');
+        infoField.innerHTML = "<b>"+message+"</b>";
+
+        if(withTimeout) {
+            setTimeout(() => {
+                infoField.classList.add('hide');
+            }, 1000);
         }
-
-        userCards.forEach(card => {
-            userCardContainer.appendChild(card.CardHTML)
-        })
-
-        oponentCards.forEach(card => {
-            oponentCardContainer.appendChild(card.CardHTML)
-        })
-
-        playerCardCountText.innerText = userCards.length;
-        oponentCardCountText.innerText = oponentCards.length;
     }
 
     function hitCards(userCards, oponentCards) {
         let userCardData = userCards.shift();
         let oponentCardData = oponentCards.shift();
         
-        if(playerField.innerHTML != '') {
-            playerField.innerHTML = ''
-        }
-
-        if(userField.innerHTML != '') {
-            userField.innerHTML = ''
-        }
-
+        clearFiledCards();
 
         createCardElement(userCardData.CardHTML.innerHTML, playerField)
         userCardValue1 = getCardValue(userCardData.Value)
@@ -108,6 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
                 drawCardsForTake = []
             }
+            showInfo("Player Win");
         }
         else if(userCardValue1 < oponentCardValue) {
             oponentCards.push(oponentCardData, userCardData);
@@ -117,9 +104,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
                 drawCardsForTake = []
             }
+            showInfo("Oponent Win");
         }
         else {
             drawCardsForTake.push(oponentCardData, userCardData);
+            showInfo("Draw, cards in stack!");
         }
 
         return [userCards, oponentCards]
@@ -197,14 +186,14 @@ document.addEventListener('DOMContentLoaded', function() {
                     break;
                 case "diamonds":
                     contnetData = "&#9830;";
-                    color = 'red';
+                    color = 'clr-red';
                     break;
                 case "clubs":
                     contnetData = "&#9827;";
                     break;
                 case "hearts":
                     contnetData = "&#9829;";
-                    color = 'red';
+                    color = 'clr-red';
                     break;
             }
 
@@ -229,6 +218,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 oponentCards.push(card)
             }
         });
+
+        // Initial card count setup
+        playerCardCountText.innerText = userCards.length;
+        oponentCardCountText.innerText = oponentCards.length;
     }
 
     function checkLoseWin() {
@@ -244,7 +237,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         if(gameEnded) {
-            alert(msg)
+            showInfo(msg, false);
+            clearFiledCards();
         }
         
         return gameEnded;
